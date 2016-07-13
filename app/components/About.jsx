e57c9e7bfbe078c13dc20196ceed52d081a9aa13
Jeremy Lincoln <bossco28@gmail.com>
2016-07-13 15:29:29 -0400

    Style about page and link to resourses

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -14,7 +14,7 @@ var About =  (props) => {
     return(
         <div>
             <h1 className="text-center">About</h1>
-            <p>This is a weather application build with React. I have built this for The Complete React Web App Developer Course</p>
+            <p>This is a weather application built with ReactJS. I have built this for The Complete React Web App Developer Course</p>
             <p>Some of the tools I used:</p>
             <ul>
                 <li><a href="https://facebook.github.io/react/">ReactJS</a></li>
