c7baaab8ee7280bf679819a3e80eb0c1405b4953
Jeremy Lincoln <bossco28@gmail.com>
2016-07-13 15:15:34 -0400

    Added in a grid layout

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var Examples = React.createClass({
 //     render: function() {
@@ -11,8 +12,17 @@ var React = require('react');
 var Examples = (props) => {
     return(
         <div>
-            <h3>Examples</h3>
-            <p>Welcome to examples page</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Flint'>Flint, MI</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Austin'>Austin, TX</Link>
+                </li>
+
+            </ol>
         </div>
     );
 };
